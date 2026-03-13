16c631a5df421216692218a375dd636113775f1f
dummylvl100-collab <dummylvl100@gmail.com>
2026-03-13 11:25:09 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()]
+        plugins: [tailwindcss()],
+        server: {
+            allowedHosts: true
+        }
     },
     integrations: [react()],
     adapter: netlify({
